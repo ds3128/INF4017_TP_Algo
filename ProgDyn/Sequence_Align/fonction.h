01d30cf21c8495fa8dce8e6a082d65dcbc741b19
ds3128 <86129628+ds3128@users.noreply.github.com>
2022-11-20 23:57:12 +0100

    sequences avec le score du meileure alignement

--- ProgDyn/Sequence_Align/fonction.h
+++ ProgDyn/Sequence_Align/fonction.h
@@ -3,6 +3,7 @@
 
 int **creerMatrice(int n, int m);
 int **sequences(char X[], char Y[], int n, int m);
+int sequences2(char X[], char Y[], int n, int m);
 std::string alignement(char X[], char Y[], int **C, std::string A, std::string B, int i, int j);
 
 #endif // FONCTION_H_INCLUDED
--- ProgDyn/Sequence_Align/main.cpp
+++ ProgDyn/Sequence_Align/main.cpp
@@ -34,7 +34,7 @@ int main()
     int **C = sequences(X,Y,n,m);
     std::string align = alignement(X, Y, C, "", "", n, m);
 
-    cout << "Le score du meilleur alignement : " << (align.length()-1)/2 << endl;
+    cout << "Le score du meilleur alignement : " << sequences2(X,Y,n,m) << endl;
     cout << "\n\nMeilleur alignement : \n\n" << align;
 
     return 0;
@@ -60,7 +60,7 @@ int **sequences(char X[], char Y[], int n, int m){
 
     int **S = creerMatrice(n+1, m+1);
     int **C = creerMatrice(n+1, m+1);
-    int score_gap = -4 , score_match =4 , score_missmatch = -4;
+    int score_gap = -4 , score_match = 4 , score_missmatch = -4;
     S[0][0] = 0;
     for(int i = 1; i < n+1; i++){ // gap qui descent
         S[i][0] = S[i-1][0] + score_gap;
@@ -115,6 +115,39 @@ int **sequences(char X[], char Y[], int n, int m){
 
     return C;
 
+}
+
+int sequences2(char X[], char Y[], int n, int m){
+
+    int **S = creerMatrice(n+1, m+1);
+    int score_gap = -4 , score_match =4 , score_missmatch = -4;
+    S[0][0] = 0;
+    for(int i = 1; i < n+1; i++){ // gap qui descent
+        S[i][0] = S[i-1][0] + score_gap;
+    }
+    for(int j = 1; j < m+1; j++){ // gap qui part de la gauche vers la droite
+        S[0][j] = S[0][j-1] + score_gap;
+    }
+    for(int i = 1; i < n + 1; i++){
+        for(int j = 1; j < m + 1; j++){
+            S[i][j] = S[i-1][j] + score_gap; // gap qui descent
+
+            if(S[i][j] < S[i][j-1]){  // gap qui part de la gauche vers la droite
+                S[i][j] = S[i][j-1] + score_gap;
+            }
+
+            if(S[i][j] < S[i-1][j-1]){
+
+                if(X[i-1] == Y[j-1]){
+                    S[i][j] = S[i-1][j-1] + score_match;
+                }
+                else {
+                    S[i][j] = S[i-1][j-1] + score_missmatch;
+                }
+            }
+        }
+    }
+    return S[n][m];
 }
 int **creerMatrice(int n, int m) {
     int **M = (int **)malloc(n*sizeof(M));
