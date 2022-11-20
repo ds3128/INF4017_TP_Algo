bd8a6477eaa41bb56f0cf2580c10c56cdc6e5f20
ds3128 <86129628+ds3128@users.noreply.github.com>
2022-11-20 10:28:49 +0100

    ok

---
+++ ProgDyn/multiplication_chaine_matrice/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include "solut.h"
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+    cout << "solution optimale retournee sous forme de chaine de caractere.. " << endl;
+    cout << "Entrez le nombre de matrices a multiplier: ";
+    cin >>n;
+    int *p = new int[n+1];
+
+    cout  << "contenu du tableau en parametre: ";
+
+    for(int i = 0; i < n+1; i++)
+    {
+        cout <<"Donnez la dimension de n"<<i<< endl;
+        cin >> p[i];
+
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << " A" << i+1;
+
+    }
+    cout  << endl;
+
+    unsigned int **s = chaine(p, n+1);
+    cout  << "retour de la suite: ";
+    cout << s[1][n] << endl;
+    cout << endl;
+
+    cout << endl << parenthesage(s, 1, n) << endl;
+
+    return 0;
+}
+
+unsigned int **creerMatrice(int n) {
+    unsigned int **m = (unsigned int **)malloc(n*sizeof(m));
+    for(int i = 0; i<n ; i++)
+    {
+        m[i] = (unsigned int *)malloc(n*sizeof(m));
+    }
+    return m;
+}
+
+unsigned int **chaine( int p[], int n )
+{
+    unsigned int q, j;
+    unsigned int **m = creerMatrice(n), **s = creerMatrice(n);
+    for(int i = 0; i < n; i++)
+        m[i][i] = 0;
+    for(int l = 2; l <= n; l++)
+    {
+        for(int i = 0; i < n-l+1; i++)
+        {
+            j = i+l-1;
+            m[i][j] = UINT_MAX;
+            for(unsigned int k = i; k < j; k++)
+            {
+                q = m[i][k] + m[k+1][j] + p[i-1]*p[k]*p[j];
+                if( q < m[i][j])
+                {
+                    m[i][j] = q;
+                    s[i][j] = k;
+                }
+            }
+        }
+    }
+
+    return s;
+}
+
+string parenthesage(unsigned int **s, int i, int j)
+{
+    int k;
+    if( i == j )
+        return  " A" + std::to_string(i);
+    else
+    {
+        k = s[i][j];
+        return " ( " + parenthesage(s, i, k) + parenthesage(s, k+1, j) + " ) ";
+    }
+}
---
+++ ProgDyn/multiplication_chaine_matrice/solut.h
@@ -0,0 +1,8 @@
+#ifndef SOLUT_H_INCLUDED
+#define SOLUT_H_INCLUDED
+
+unsigned int **chaine(int p[], int n);
+unsigned int **creerMatrice(int n);
+std::string parenthesage(unsigned int **s, int i, int j);
+
+#endif // SOLUT_H_INCLUDED
