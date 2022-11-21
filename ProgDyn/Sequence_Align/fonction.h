728919a3555da5dd40539894877ff964fcc73ce7
Blaise-dev <83373351+Blaise-dev@users.noreply.github.com>
2022-11-21 06:32:05 +0100

    Delete ProgDyn/Sequence_Align directory

--- ProgDyn/Sequence_Align/fonction.h
+++
@@ -1,9 +0,0 @@
-#ifndef FONCTION_H_INCLUDED
-#define FONCTION_H_INCLUDED
-
-int **creerMatrice(int n, int m);
-int **sequences(char X[], char Y[], int n, int m);
-int sequences2(char X[], char Y[], int n, int m);
-std::string alignement(char X[], char Y[], int **C, std::string A, std::string B, int i, int j);
-
-#endif // FONCTION_H_INCLUDED
