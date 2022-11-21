3f1ffb623dc5b498d34dea0619a27039ba7d32b4
Blaise-dev <83373351+Blaise-dev@users.noreply.github.com>
2022-11-21 07:02:33 +0100

    Update of Sequence Align Global.

---
+++ ProgDyn/Global_Sequence_Align/fonction.h
@@ -0,0 +1,11 @@
+#ifndef FONCTION_H_INCLUDED
+#define FONCTION_H_INCLUDED
+
+using namespace std;
+
+int **creerMatrice(int n, int m);
+int **sequences(string X, string Y);
+int score_alignement(string X, string Y);
+string alignement(string X, string Y, int **C, string A, string B, int i, int j);
+
+#endif // FONCTION_H_INCLUDED
